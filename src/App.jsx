760ae7a4c6a9b8d3c6279f4efe5b237419491ed4
Austin Buckley <austinbuckley2023@gmail.com>
2024-06-16 14:26:03 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function Header() {
+  return (
+    <>
+      <h1 className="sushi">Sakura</h1>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
+}
+
+export default App;
